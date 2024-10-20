83cbdab10f9b32b9331c7841d3c939e6df7d2eb0
zzhakerMikael <mihailpokidov2008@yandex.ru>
2024-10-20 18:57:10 +0700

    455
    YYi

---
+++ main.py
@@ -0,0 +1,45 @@
+#Импорт
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    #Переменные для энергозатратности приборов
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+#Первая страница
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#Вторая страница
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+#Третья страница
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',
+                            size = size, 
+                            lights = lights                           
+                           )
+
+#Расчет
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+app.run(debug=True)
